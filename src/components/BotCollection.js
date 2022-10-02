117d347ce9b5819fd999f34c1df5fd712422d599
kapadokia <ktnyandoch@gmail.com>
2022-10-02 11:55:48 +0300

    step one done

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import BotCard from "./BotCard";
 
 function BotCollection() {
-  // Your code here
+  const [botArmy, setBotArmy] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:8002/bots")
+    .then(res=> res.json())
+    .then(data =>{
+      setBotArmy(data)
+    })
+  },[])
+
+  
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
+        {console.log(botArmy)}
+        {botArmy.map(data =>(
+          <BotCard bot={data}/>
+        ))}
       </div>
     </div>
   );
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function YourBotArmy() {
-  //your bot army code here...
+ 
+
 
   return (
     <div className="ui segment inverted olive bot-army">
